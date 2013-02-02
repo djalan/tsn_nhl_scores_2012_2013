f891b186755b89f0a2483eeb89322cab8e4e5c7a
Alain <alainsirois@gmail.com>
2013-02-02 01:50:17 -0500

    working!

---
+++ do.rb
@@ -0,0 +1,102 @@
+#!/usr/bin/env ruby
+
+
+require 'nokogiri'
+require 'open-uri'
+
+
+# ==============
+# Download games
+# ==============
+scores_url = "http://www.tsn.ca/nhl/scores"
+scores_file = "scores.html"
+boxscore_url = "http://www.tsn.ca/nhl/scores/boxscore/?id="
+
+File::open(scores_file, 'w') do |f|
+	f << open(scores_url).read
+end
+
+html = Nokogiri::HTML(File.open(File.expand_path(scores_file)))
+games_list = []
+
+html.css('div#tsnMain div.alignRight a').select { |link| link['href'] =~ %r{/nhl/scores/boxscore/} }.each do |link|
+	game_id = link['href'].split('=')[-1]
+	games_list << game_id
+	File::open("#{game_id}.html", 'w') do |f|
+		f << open("#{boxscore_url}#{game_id}").read
+	end
+end
+
+
+# =================
+# Define my players
+# =================
+my_defenders = ['Joe Corvo', 'Tobias Enstrom', 'Erik Karlsson', 'Dustin Bufyglien', 'Michael Del Zotto', 'John-Michael Liles']
+
+my_forwards = ['Travis Zajac', 'Joe Pavelski', 'Logan Couture', 'Tyler Seguin',
+	'Curtis Glencross', 'Tyler Ennis', 'Milan Lucic', 'Michael Ryder', 'Kyle Okposo', 'Scott Hartnell', 'Justin Williams', 'Joffrey Lupul']
+
+my_goalers = ['Jonathan Quick', 'Jimmy Howard']
+
+my_skaters = my_defenders + my_forwards
+
+my_players = my_skaters + my_goalers
+
+
+# ================
+# Print statistics
+# ================
+goal_total = 0
+assist_total = 0
+fantasy_points_total = 0
+goalers_status = []
+
+puts "G  A  F  Players"
+puts "----------------"
+games_list.each do |game|
+	html = Nokogiri::HTML(File.open(File.expand_path("#{game}.html")))
+	
+	playing = html.css('div#tsnStats table.siPlayerBoxStats tr').select { |tr| my_players.include? tr.css('a').text }
+
+	p_skaters = playing.select { |tr| my_skaters.include? tr.css('a').text }
+	p_goalers = playing.select { |tr| my_goalers.include? tr.css('a').text }
+	
+	goalers_status << p_goalers	
+	
+	#p_goalers.each { |tr| puts "#{tr.css('a').text} is playing tonight" }
+	
+	p_skaters.each do |tr|
+		children = tr.children
+
+		name = tr.css('a').text
+		goal = children[1].text.to_i
+		assist = children[2].text.to_i
+
+		if my_defenders.include? name
+			fantasy_goal = goal * 3
+			fantasy_assist  = assist * 2
+		elsif my_forwards.include? name
+			fantasy_goal = goal * 2
+			fantasy_assist  = assist * 1
+		end
+		fantasy_points = fantasy_goal + fantasy_assist
+		goal_total += goal
+		assist_total += assist
+		fantasy_points_total += fantasy_points
+
+		puts "#{goal}g #{assist}a #{fantasy_points}f #{name}"
+	end
+end
+
+puts "----------------"
+puts "#{'%02d' % goal_total} #{'%02d' % assist_total} #{'%02d' % fantasy_points_total} Total"
+if goalers_status.nil?
+	puts 'No goalie playing tonight'
+else	
+	goalers_status.each do |p_goaler|
+		p_goaler.each { |tr| puts "#{tr.css('a').text} is playing tonight" }
+	end
+end
+
+
+__END__
